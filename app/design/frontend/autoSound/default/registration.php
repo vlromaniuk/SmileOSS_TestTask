532888177b35eacb5db4d278dc17165556b9cb24
vlromaniuk <vlad96r@protonmail.com>
2020-03-23 01:53:55 +0200

    init repo and add theme

---
+++ app/design/frontend/autoSound/default/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * autoSound default.
+ * @author Vladislav Romaniuk vlad96r@protonmial.com
+ * @copyright 2020 Vlromaniuk
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/autoSound/default', __DIR__);
