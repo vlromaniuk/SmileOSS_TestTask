ed9a7755a271d7c9898c7701c8c7aa19f5f13227
Vladislav Romaniuk <vlad96r@protonmail.com>
2020-04-04 10:58:29 +0300

    fix comments

--- app/code/AutoSound/Expand/registration.php
+++ app/code/AutoSound/Expand/registration.php
@@ -1,4 +1,11 @@
 <?php
+/**
+autoSound Expand registration.
+@author Vladislav Romaniuk vlad96r@protonmial.com
+@copyright 2020 Vlromaniuk
+ */
+?>
+<?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
--- app/code/AutoSound/PriceCurrency/registration.php
+++ app/code/AutoSound/PriceCurrency/registration.php
@@ -1,4 +1,11 @@
 <?php
+/**
+autoSound PriceCurrency registration.
+@author Vladislav Romaniuk vlad96r@protonmial.com
+@copyright 2020 Vlromaniuk
+ */
+?>
+<?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
